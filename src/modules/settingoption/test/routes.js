7a5a3685cbe9329fdc37ec3154b67fea913b4a75
Natsulelou <nutshapon_lelou@hotmail.com>
2019-12-03 10:55:51 +0700

    first

---
+++ src/modules/settingoption/test/routes.js
@@ -0,0 +1,228 @@
+'use strict';
+var request = require('supertest'),
+    assert = require('assert'),
+    config = require('../../../config/config'),
+    _ = require('lodash'),
+    jwt = require('jsonwebtoken'),
+    mongoose = require('mongoose'),
+    app = require('../../../config/express'),
+    Settingoption = mongoose.model('Settingoption');
+
+var credentials,
+    token,
+    mockup;
+
+describe('Settingoption CRUD routes tests', function () {
+
+    before(function (done) {
+        mockup = {
+            name: 'name'
+        };
+        credentials = {
+            username: 'username',
+            password: 'password',
+            firstname: 'first name',
+            lastname: 'last name',
+            email: 'test@email.com',
+            roles: ['user']
+        };
+        token = jwt.sign(_.omit(credentials, 'password'), config.jwt.secret, {
+            expiresIn: 2 * 60 * 60 * 1000
+        });
+        done();
+    });
+
+    it('should be Settingoption get use token', (done)=>{
+        request(app)
+        .get('/api/settingoptions')
+        .set('Authorization', 'Bearer ' + token)
+        .expect(200)
+        .end((err, res)=>{
+            if (err) {
+                return done(err);
+            }
+            var resp = res.body;
+            done();
+        });
+    });
+
+    it('should be Settingoption get by id', function (done) {
+
+        request(app)
+            .post('/api/settingoptions')
+            .set('Authorization', 'Bearer ' + token)
+            .send(mockup)
+            .expect(200)
+            .end(function (err, res) {
+                if (err) {
+                    return done(err);
+                }
+                var resp = res.body;
+                request(app)
+                    .get('/api/settingoptions/' + resp.data._id)
+                    .set('Authorization', 'Bearer ' + token)
+                    .expect(200)
+                    .end(function (err, res) {
+                        if (err) {
+                            return done(err);
+                        }
+                        var resp = res.body;
+                        assert.equal(resp.status, 200);
+                        assert.equal(resp.data.name, mockup.name);
+                        done();
+                    });
+            });
+
+    });
+
+    it('should be Settingoption post use token', (done)=>{
+        request(app)
+            .post('/api/settingoptions')
+            .set('Authorization', 'Bearer ' + token)
+            .send(mockup)
+            .expect(200)
+            .end(function (err, res) {
+                if (err) {
+                    return done(err);
+                }
+                var resp = res.body;
+                assert.equal(resp.data.name, mockup.name);
+                done();
+            });
+    });
+
+    it('should be settingoption put use token', function (done) {
+
+        request(app)
+            .post('/api/settingoptions')
+            .set('Authorization', 'Bearer ' + token)
+            .send(mockup)
+            .expect(200)
+            .end(function (err, res) {
+                if (err) {
+                    return done(err);
+                }
+                var resp = res.body;
+                var update = {
+                    name: 'name update'
+                }
+                request(app)
+                    .put('/api/settingoptions/' + resp.data._id)
+                    .set('Authorization', 'Bearer ' + token)
+                    .send(update)
+                    .expect(200)
+                    .end(function (err, res) {
+                        if (err) {
+                            return done(err);
+                        }
+                        var resp = res.body;
+                        assert.equal(resp.data.name, update.name);
+                        done();
+                    });
+            });
+
+    });
+
+    it('should be settingoption delete use token', function (done) {
+
+        request(app)
+            .post('/api/settingoptions')
+            .set('Authorization', 'Bearer ' + token)
+            .send(mockup)
+            .expect(200)
+            .end(function (err, res) {
+                if (err) {
+                    return done(err);
+                }
+                var resp = res.body;
+                request(app)
+                    .delete('/api/settingoptions/' + resp.data._id)
+                    .set('Authorization', 'Bearer ' + token)
+                    .expect(200)
+                    .end(done);
+            });
+
+    });
+
+    it('should be settingoption get not use token', (done)=>{
+        request(app)
+        .get('/api/settingoptions')
+        .expect(403)
+        .expect({
+            status: 403,
+            message: 'User is not authorized'
+        })
+        .end(done);
+    });
+
+    it('should be settingoption post not use token', function (done) {
+
+        request(app)
+            .post('/api/settingoptions')
+            .send(mockup)
+            .expect(403)
+            .expect({
+                status: 403,
+                message: 'User is not authorized'
+            })
+            .end(done);
+
+    });
+
+    it('should be settingoption put not use token', function (done) {
+
+        request(app)
+            .post('/api/settingoptions')
+            .set('Authorization', 'Bearer ' + token)
+            .send(mockup)
+            .expect(200)
+            .end(function (err, res) {
+                if (err) {
+                    return done(err);
+                }
+                var resp = res.body;
+                var update = {
+                    name: 'name update'
+                }
+                request(app)
+                    .put('/api/settingoptions/' + resp.data._id)
+                    .send(update)
+                    .expect(403)
+                    .expect({
+                        status: 403,
+                        message: 'User is not authorized'
+                    })
+                    .end(done);
+            });
+
+    });
+
+    it('should be settingoption delete not use token', function (done) {
+
+        request(app)
+            .post('/api/settingoptions')
+            .set('Authorization', 'Bearer ' + token)
+            .send(mockup)
+            .expect(200)
+            .end(function (err, res) {
+                if (err) {
+                    return done(err);
+                }
+                var resp = res.body;
+                request(app)
+                    .delete('/api/settingoptions/' + resp.data._id)
+                    .expect(403)
+                    .expect({
+                        status: 403,
+                        message: 'User is not authorized'
+                    })
+                    .end(done);
+            });
+
+    });
+
+    afterEach(function (done) {
+        Settingoption.remove().exec(done);
+    });
+
+});
